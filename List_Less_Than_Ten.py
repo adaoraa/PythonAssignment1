9a488ff10b179b980157d05fc008d70993605d54
Adaora Atuegbu <aatuegbu@tamu.edu>
2016-05-17 10:24:36 -0500

    List Less Than 10

--- List_Less_Than_Ten.py
+++ List_Less_Than_Ten.py
@@ -1,8 +1,7 @@
 
 #Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-#and write a program that prints out all the elements of the list that
-#  are less than 5.
+#and write a program that prints out all the elements of the list that are less than 5.
 
 a=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b=[x for x in a if x < 5] #creates new list of integers < 5
-print(b)
+output_list=[i for i in a if i < 5] #creates new list of integers < 5
+print(output_list)
