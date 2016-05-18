cd2a2414392222ceeb3c956498062b1c985363f7
Adaora Atuegbu <aatuegbu@tamu.edu>
2016-05-18 00:28:24 -0500

    Update CharacterInput.py

--- CharacterInput.py
+++ CharacterInput.py
@@ -4,13 +4,6 @@ import datetime
 # Print out a message addressed to them that tells them the year that
 # they will turn 100 years old.
 
-#1. Add on to the previous program by asking the user for another number
-# and printing out that many copies of the previous message.
-# (Hint: order of operations exists in Python)
-
-#2. Print out that many copies of the previous message on separate lines.
-# (Hint: the string "\n is the same as pressing the ENTER button)
-
 name = input('What is your name?:')
 print('My name is ' +name)
 age = input('How old are you?:')
