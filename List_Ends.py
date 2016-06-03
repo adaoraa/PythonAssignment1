cc186da7e7131f2e791b0bc50a40c2becb67c5bd
Adaora Atuegbu <aatuegbu@tamu.edu>
2016-06-03 00:30:56 -0500

    Add files via upload

---
+++ List_Ends.py
@@ -0,0 +1,10 @@
+# Write a program that takes a list of numbers
+# (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only
+# the first and last elements of the given list.
+# For practice, write this code inside a function.
+
+
+def main(user_list):
+    print(user_list[0],user_list[-1])  # # returns first and last elements
+
+main([5, 10, 15, 20, 25])  # calls functions
