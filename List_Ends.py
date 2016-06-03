068334f98bc6ee4d78deb4f494fbdaa6211289a9
Adaora Atuegbu <aatuegbu@tamu.edu>
2016-06-03 00:44:08 -0500

    Add files via upload

--- List_Ends.py
+++ List_Ends.py
@@ -4,7 +4,11 @@
 # For practice, write this code inside a function.
 
 
-def main(user_list):
-    print(user_list[0],user_list[-1])  # # returns first and last elements
-
-main([5, 10, 15, 20, 25])  # calls functions
+num1 = int(input('Enter a number: '))  # prompts user to input number representing first element of list
+num2 = int(input('Enter a number: '))  # prompts user to input number representing second element of list
+num3 = int(input('Enter a number: '))  # prompts user to input number representing third element of list
+num4 = int(input('Enter a number: '))  # prompts user to input number representing fourth element of list
+num5 = int(input('Enter a number: '))  # prompts user to input number representing fifth element of list
+user_list = [num1, num2, num3, num4, num5]  # creates list of integers
+print(user_list)
+print(user_list[0], user_list[-1])  # returns first and last elements of list
